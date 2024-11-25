3902c3ff97932b9a5c804fc2c208d21ee6f10f55
hannahabell0911 <hannahabell2000@gmail.com>
2024-11-25 13:49:42 +0000

    #56779- dependencies-  Add Firebase dependencies for authentication and database integration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -40,4 +41,7 @@ dependencies {
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
     implementation(libs.core.ktx)
+    implementation(platform("com.google.firebase:firebase-bom:33.6.0"))
+    implementation("com.google.firebase:firebase-analytics")
+
 }
