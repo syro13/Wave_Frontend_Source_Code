060bf2d482f4b79f31d0b4f2925ffdb80aa3fddd
Raveena Rathnam <118574384+RaveenaRathnam@users.noreply.github.com>
2024-12-18 17:38:38 +0000

    #56745 - Feature - Create blog model and blog adapter

--- app/src/main/java/com/example/wave/Blog.java
+++ app/src/main/java/com/example/wave/Blog.java
@@ -1,4 +1,20 @@
 package com.example.wave;
 
 public class Blog {
+    private String title;
+    private String author;
+
+    public Blog(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
 }
+
