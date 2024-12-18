06b38250c4e3a8136c0f67bd342e40d379663614
Raveena Rathnam <118574384+RaveenaRathnam@users.noreply.github.com>
2024-12-18 18:18:12 +0000

    #56745 - Feature - Implement BlogAdapter.java

--- app/src/main/java/com/example/wave/BlogAdapter.java
+++ app/src/main/java/com/example/wave/BlogAdapter.java
@@ -1,4 +1,51 @@
 package com.example.wave;
 
-public class BlogAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.List;
+
+public class BlogAdapter extends RecyclerView.Adapter<BlogAdapter.BlogViewHolder> {
+
+    private List<Blog> blogs;
+
+    public BlogAdapter(List<Blog> blogs) {
+        this.blogs = blogs;
+    }
+
+    @NonNull
+    @Override
+    public BlogViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.blog_item_card, parent, false);
+        return new BlogViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull BlogViewHolder holder, int position) {
+        Blog blog = blogs.get(position);
+        holder.title.setText(blog.getTitle());
+        holder.author.setText(blog.getAuthor());
+        // Add more logic for blog image or other actions
+    }
+
+    @Override
+    public int getItemCount() {
+        return blogs.size();
+    }
+
+    static class BlogViewHolder extends RecyclerView.ViewHolder {
+        TextView title, author;
+        ImageView image;
+
+        public BlogViewHolder(@NonNull View itemView) {
+            super(itemView);
+            title = itemView.findViewById(R.id.blogTitle);
+            author = itemView.findViewById(R.id.blogAuthor);
+            image = itemView.findViewById(R.id.blogImage);
+        }
+    }
 }
