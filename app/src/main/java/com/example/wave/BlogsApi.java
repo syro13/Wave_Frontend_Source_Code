f561b41b0f6a4b69405096f2548c3e825ba5dda1
Raveena Rathnam <118574384+RaveenaRathnam@users.noreply.github.com>
2024-12-27 16:55:06 +0000

    #56745 - Feature - Update BlogsApi to keep the api key secure.

--- app/src/main/java/com/example/wave/BlogsApi.java
+++ app/src/main/java/com/example/wave/BlogsApi.java
@@ -9,21 +9,19 @@ import retrofit2.http.Query;
 
 public interface BlogsApi {
 
-    @Headers({
-            "x-rapidapi-key: 29917425cdmsh96de0dd42300380p175d46jsn3a36125902cf", // Replace with your API key
-            "x-rapidapi-host: medium2.p.rapidapi.com"
-    })
-    @GET("recommended_feed/{tag}")
-    Call<RecommendedFeedResponse> getRecommendedFeed(
-            @Path("tag") String tag,
-            @Query("page") int page
-    );
-    @Headers({
-            "x-rapidapi-key: 29917425cdmsh96de0dd42300380p175d46jsn3a36125902cf", // Replace with your API key
-            "x-rapidapi-host: medium2.p.rapidapi.com"
-    })
-    @GET("article/{article_id}")
-    Call<ArticleDetails> getArticleInfo(
-            @Path("article_id") String articleId
-    );
-}
+        @GET("recommended_feed/{tag}")
+        Call<RecommendedFeedResponse> getRecommendedFeed(
+                @Path("tag") String tag,
+                @Query("page") int page
+        );
+
+        @GET("article/{article_id}")
+        Call<ArticleDetails> getArticleInfo(
+                @Path("article_id") String articleId
+        );
+
+        @GET("user/{user_id}")
+        Call<UserInfo> getUserInfo(
+                @Path("user_id") String userId
+        );
+    }
