14492211dc1f17f7453bf688404f9468c2902622
hannahabell0911 <96234964+hannahabell0911@users.noreply.github.com>
2025-01-27 12:38:54 +0000

    #56120 - Feature - Added dependencies for high sdk for calendar

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.wave"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
@@ -27,11 +27,19 @@ android {
         }
     }
     compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+        buildFeatures {
+            viewBinding = true
+        }
+
+
+    compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-
-
 }
 
 dependencies {
@@ -48,11 +56,13 @@ dependencies {
     implementation(platform(libs.firebase.bom))
     implementation(libs.firebase.analytics)
     implementation (libs.play.services.auth.v2070)
+    coreLibraryDesugaring (libs.desugar.jdk.libs)
     implementation ("com.google.android.material:material:1.9.0")
     implementation (libs.recyclerview)
     implementation (libs.retrofit)
     implementation (libs.converter.gson)
     implementation (libs.logging.interceptor)
+    implementation ("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation ("com.github.bumptech.glide:glide:4.15.1")
     annotationProcessor ("com.github.bumptech.glide:compiler:4.15.1")
     implementation (libs.logging.interceptor.v493)
