4997eb2cb31a9103fd97464f2450ea8b0d9cf340
NojusMat <d00249674@student.dkit.ie>
2025-03-16 16:56:28 +0000

    Comment out CalenderDApterUnitTest

--- app/src/test/java/com/example/wave/CalendarAdapterUnitTests.java
+++ app/src/test/java/com/example/wave/CalendarAdapterUnitTests.java
@@ -1,46 +1,46 @@
-package com.example.wave;
-
-import static junit.framework.TestCase.assertEquals;
-import static org.mockito.Mockito.*;
-
-import android.content.Context;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
-
-import androidx.core.content.ContextCompat;
-import androidx.recyclerview.widget.RecyclerView;
-
-import org.junit.Before;
-import org.junit.Test;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
-public class CalendarAdapterUnitTests {
-//    private CalendarAdapter adapter;
-//    private Set<String> schoolTaskDates;
-//    private Set<String> homeTaskDates;
-//    private List<String> calendarDates;
+//package com.example.wave;
 //
-//    @Before
-//    public void setUp() {
-//        calendarDates = Arrays.asList("01-03-2024", "02-03-2024", "03-03-2024");
-//        schoolTaskDates = new HashSet<>(Arrays.asList("01-03-2024"));
-//        homeTaskDates = new HashSet<>(Arrays.asList("05-03-2024"));
+//import static junit.framework.TestCase.assertEquals;
+//import static org.mockito.Mockito.*;
 //
-//        adapter = new CalendarAdapter(calendarDates, null, schoolTaskDates, homeTaskDates);
-//    }
+//import android.content.Context;
+//import android.view.View;
+//import android.view.ViewGroup;
+//import android.widget.TextView;
 //
-//    @Test // Does not work
-//    public void testUpdateData() {
-//        List<String> newDates = Arrays.asList("05-03-2024", "06-03-2024");
-//        adapter.updateData(newDates);
-//        adapter.notifyDataSetChanged();
-//        assertEquals( 2, adapter.getItemCount());
-//    }
-}
+//import androidx.core.content.ContextCompat;
+//import androidx.recyclerview.widget.RecyclerView;
+//
+//import org.junit.Before;
+//import org.junit.Test;
+//import org.mockito.Mock;
+//import org.mockito.MockitoAnnotations;
+//
+//import java.util.Arrays;
+//import java.util.HashSet;
+//import java.util.List;
+//import java.util.Set;
+//
+//public class CalendarAdapterUnitTests {
+////    private CalendarAdapter adapter;
+////    private Set<String> schoolTaskDates;
+////    private Set<String> homeTaskDates;
+////    private List<String> calendarDates;
+////
+////    @Before
+////    public void setUp() {
+////        calendarDates = Arrays.asList("01-03-2024", "02-03-2024", "03-03-2024");
+////        schoolTaskDates = new HashSet<>(Arrays.asList("01-03-2024"));
+////        homeTaskDates = new HashSet<>(Arrays.asList("05-03-2024"));
+////
+////        adapter = new CalendarAdapter(calendarDates, null, schoolTaskDates, homeTaskDates);
+////    }
+////
+////    @Test // Does not work
+////    public void testUpdateData() {
+////        List<String> newDates = Arrays.asList("05-03-2024", "06-03-2024");
+////        adapter.updateData(newDates);
+////        adapter.notifyDataSetChanged();
+////        assertEquals( 2, adapter.getItemCount());
+////    }
+//}
